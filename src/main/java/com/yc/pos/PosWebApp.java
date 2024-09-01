b65dab03c60f9e6042dca7eb6f45c12de538528a
RanjitPatil <patilranjit485@gmail.com>
2024-09-01 13:36:48 +0530

    Person DaoImpl and Service layer added all methods are  tested

--- src/main/java/com/yc/pos/PosWebApp.java
+++ src/main/java/com/yc/pos/PosWebApp.java
@@ -2,31 +2,45 @@ package com.yc.pos;
 
 import com.yc.pos.config.ApplicationConfig;
 import com.yc.pos.config.HibernateConfig;
-import com.yc.pos.dao.PersonDaoImpl;
+import com.yc.pos.interfaces.PersonDao;
 import com.yc.pos.model.Person;
+import com.yc.pos.service.PersonService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Scanner;
+
 public class PosWebApp {
     public static void main(String[] args) {
+        // Initialize the Spring application context with the required configurations
         ApplicationContext context = new AnnotationConfigApplicationContext(HibernateConfig.class, ApplicationConfig.class);
-        PersonDaoImpl personDaoImpl = context.getBean("personDaoImpl",PersonDaoImpl.class);
+
+        // Get the bean using the interface type
+
+        PersonService personService = context.getBean(PersonService.class);
+
+        // Create a new Person instance and set its properties
 
         Person p = new Person();
         p.setAddress1("Bhivghat");
-        p.setCity("sangli");
+        p.setCity("Sangli");
         p.setComments("hello");
         p.setEmail("patilranjit485@gmail.com");
-        p.setFirstName("ranjit");
+        p.setFirstName("Ranjit");
         p.setGender(1);
-        p.setState("mh");
+        p.setState("MH");
         p.setLastName("Patil");
         p.setPersonId(88);
         p.setZip("66666");
-        p.setCountry("india");
+        p.setCountry("India");
         p.setPhoneNumber("9999777777");
-        p.setAddress2("Hm ghat news news vita");
+        p.setAddress2("Hm ghat news news Vita");
 
-        personDaoImpl.save(p);
+        // Use the DAO to save the Person instance
+//        personDao.save(p);
+
+
+
+
     }
 }
--- src/main/java/com/yc/pos/dao/PersonDaoImpl.java
+++ src/main/java/com/yc/pos/dao/PersonDaoImpl.java
@@ -14,6 +14,9 @@ public class PersonDaoImpl implements PersonDao {
     @Autowired
     private HibernateTemplate hibernateTemplate;
 
+    @Autowired
+    private PersonDao personDao;
+
     @Override
     public void save(Person person) {
         hibernateTemplate.save(person);
@@ -31,42 +34,69 @@ public class PersonDaoImpl implements PersonDao {
 
     @Override
     public List<Person> findByFirstName(String firstName) {
-        return null;
+        // Use HibernateTemplate's findByNamedParam method to query by firstName
+
+        String query = "FROM Person WHERE firstName = :firstName";
+        return (List<Person>) hibernateTemplate.findByNamedParam(query, "firstName", firstName);
     }
 
     @Override
     public List<Person> findByLastName(String lastName) {
-        return null;
+        String query = "FROM Person WHERE lastName = :lastName";
+        return (List<Person>) hibernateTemplate.findByNamedParam(query, "lastName", lastName);
     }
 
     @Override
     public List<Person> findByEmail(String email) {
-        return null;
+        return hibernateTemplate.execute(session -> {
+            return session.createQuery("FROM Person WHERE email = :email", Person.class)
+                    .setParameter("email", email)
+                    .getResultList();
+        });
     }
 
     @Override
     public List<Person> findByPhoneNumber(String phoneNumber) {
-        return null;
+        return hibernateTemplate.execute(session -> {
+            return session.createQuery("FROM Person WHERE phoneNumber = :phoneNumber", Person.class)
+                    .setParameter("phoneNumber", phoneNumber)
+                    .getResultList();
+        });
     }
 
     @Override
     public List<Person> findByCity(String city) {
-        return null;
-    }
+        return hibernateTemplate.execute(session -> {
+            return session.createQuery("FROM Person WHERE city = :city", Person.class)
+                    .setParameter("city", city)
+                    .getResultList();
+        });    }
 
     @Override
     public List<Person> findByState(String state) {
-        return null;
+        return hibernateTemplate.execute(session -> {
+            return session.createQuery("FROM Person WHERE state = :state", Person.class)
+                    .setParameter("state", state)
+                    .getResultList();
+        });
     }
 
     @Override
     public List<Person> findByZip(String zip) {
-        return null;
+        return hibernateTemplate.execute(session -> {
+            return session.createQuery("FROM Person WHERE zip = :zip", Person.class)
+                    .setParameter("zip", zip)
+                    .getResultList();
+        });
     }
 
     @Override
     public List<Person> findByCountry(String country) {
-        return null;
+        return hibernateTemplate.execute(session -> {
+            return session.createQuery("FROM Person WHERE country = :country", Person.class)
+                    .setParameter("country", country)
+                    .getResultList();
+        });
     }
 
 
@@ -93,12 +123,18 @@ public class PersonDaoImpl implements PersonDao {
 
     @Override
     public long count() {
-        return 0;
+        return hibernateTemplate.execute(session ->
+                (Long) session.createQuery("SELECT COUNT(p) FROM Person p").uniqueResult()
+        );
     }
-
     @Override
     public boolean existsById(int personId) {
-        return false;
+        Long count = hibernateTemplate.execute(session ->
+                (Long) session.createQuery("SELECT COUNT(p) FROM Person p WHERE p.personId = :personId")
+                        .setParameter("personId", personId)
+                        .uniqueResult()
+        );
+        return count != null && count > 0;
     }
 
 }
